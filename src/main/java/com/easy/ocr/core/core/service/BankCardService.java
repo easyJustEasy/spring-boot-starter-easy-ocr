1ea4b0da43f87923fbef2d7b3b700f18c308ee96
siyuan.zhu <siyuan.zhu@zhaopin.com.cn>
2021-02-05 15:10:52 +0800

    99

--- src/main/java/com/easy/ocr/core/core/service/BankCardService.java
+++ src/main/java/com/easy/ocr/core/core/service/BankCardService.java
@@ -6,5 +6,11 @@ import com.easy.ocr.core.core.bean.BankCard;
 import java.io.File;
 
 public interface BankCardService {
+
+    /**
+     * 解析银行卡
+     * @param img
+     * @return
+     */
     BankCard showBankNo(File img);
 }
--- src/main/java/com/easy/ocr/core/core/service/IdCardService.java
+++ src/main/java/com/easy/ocr/core/core/service/IdCardService.java
@@ -6,6 +6,18 @@ import com.easy.ocr.core.core.bean.IdCard;
 import java.io.File;
 
 public interface IdCardService {
+
+    /**
+     * 解析身份证前面
+     * @param img
+     * @return
+     */
     IdCard showFront(File img);
+
+    /**
+     * 解析身份证后面
+     * @param img
+     * @return
+     */
     IdCard showBack(File img);
 }
